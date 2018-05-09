78fdb6c9f0fb9a68b0a8df9c120bc60ade446826
ViktorSlavov <viktorslavov91@gmail.com>
2018-05-09 17:33:55 +0300

    feat(igx-combo): initial add drop-down in combo, #1260

--- src/combo/combo.component.ts
+++ src/combo/combo.component.ts
@@ -1,12 +1,26 @@
-import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, NgModule, OnDestroy, OnInit, Output } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+    ChangeDetectorRef, Component, ElementRef,
+    EventEmitter, Input, NgModule, OnDestroy, OnInit, Output, ViewChild
+} from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { IgxSelectionAPIService } from "../core/selection";
 import { IgxRippleModule } from "../directives/ripple/ripple.directive";
+import { IgxDropDownItemComponent } from "../drop-down/drop-down-item.component";
+import { IgxDropDownComponent, IgxDropDownModule } from "../drop-down/drop-down.component";
+import { IgxInputGroupModule } from "../input-group/input-group.component";
 
 @Component({
     selector: "igx-combo",
     templateUrl: "combo.component.html"
 })
 export class IgxComboComponent implements OnInit, OnDestroy {
+
+    public value = "";
+
+    @ViewChild(IgxDropDownComponent, { read: IgxDropDownComponent })
+    public dropDown: IgxDropDownComponent;
+
     @Input()
     public data;
 
@@ -34,6 +48,22 @@ export class IgxComboComponent implements OnInit, OnDestroy {
     public ngOnDestroy() {
 
     }
+
+    public onSelection(event) {
+        if (event.newSelection) {
+            if (event.newSelection !== event.oldSelection) {
+                this.value = this.data[event.newSelection.index];
+            }
+        }
+    }
+
+    public toggleDropDown(event, state?) {
+        if (state !== undefined) {
+            if (state) {
+                this.dropDown.open();
+            }
+        }
+    }
 }
 
 export interface IComboDropDownOpenEventArgs {
@@ -53,6 +83,6 @@ export interface IComboSelectionChangeEventArgs {
 @NgModule({
     declarations: [IgxComboComponent],
     exports: [IgxComboComponent],
-    imports: [IgxRippleModule]
+    imports: [IgxRippleModule, IgxDropDownModule, CommonModule, IgxInputGroupModule, FormsModule]
 })
 export class IgxComboModule { }
