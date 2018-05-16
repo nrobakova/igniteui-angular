55bcb7f1a9e6e4bbcd8859b193b5aaf1778c040c
ViktorSlavov <viktorslavov91@gmail.com>
2018-05-16 17:16:33 +0300

    chore(igx-combo): add combo-item.component.ts

---
+++ src/combo/combo-item.component.ts
@@ -0,0 +1,57 @@
+import {
+    Component,
+    ElementRef,
+    forwardRef,
+    HostBinding,
+    HostListener,
+    Inject,
+    Input
+} from "@angular/core";
+import { IgxSelectionAPIService } from "../core/selection";
+import { IgxDropDownItemTemplate } from "../drop-down/drop-down-item.component";
+import { IgxComboComponent } from "./combo.component";
+
+@Component({
+    selector: "igx-combo-item",
+    template: "<ng-content></ng-content>"
+})
+export class IgxComboItemComponent extends IgxDropDownItemTemplate {
+    /**
+     * Gets if the item is the currently selected one in the dropdown
+     */
+
+    @Input()
+    public itemData;
+
+    public get itemID() {
+        // A row in the grid is identified either by:
+        // primaryKey data value,
+        // or if the primaryKey is omitted, then the whole rowData is used instead.
+        const valueKey = this.parentElement.valueKey;
+        return valueKey ? this.itemData[valueKey] : this.itemData;
+    }
+
+    constructor(
+        @Inject(forwardRef(() => IgxComboComponent)) public parentElement: IgxComboComponent,
+        protected elementRef: ElementRef,
+        protected selectionAPI: IgxSelectionAPIService
+    ) {
+        super(parentElement, elementRef);
+    }
+
+    get isSelected() {
+        return this.parentElement.selectedItem.indexOf(this.itemID) > -1;
+    }
+
+    markItemSelected() {
+        this.parentElement.changeFocusedItem(this, this.parentElement.lastFocused);
+        this.parentElement.setSelectedItem(this.itemID);
+    }
+
+    @HostListener("keydown.Space", ["$event"])
+    onSpaceKeyDown(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        this.markItemSelected();
+    }
+}
